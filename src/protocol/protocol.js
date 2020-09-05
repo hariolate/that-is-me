9cd39b972e626938bba98d6cbf92d9b01f1ad3f5
fanghr <chfanghr@gmail.com>
2020-09-05 09:32:03 +0800

    feat: add protocols

--- src/protocol/protocol.js
+++ src/protocol/protocol.js
@@ -8,6 +8,130 @@ var $root = ($protobuf.roots["default"] || ($protobuf.roots["default"] = new $pr
   go_package: ".;protocol"
 })
 .addJSON({
+  UserATapEvent: {
+    fields: {
+      object: {
+        type: "uint32",
+        id: 1
+      }
+    }
+  },
+  Position: {
+    fields: {
+      x: {
+        type: "uint32",
+        id: 1
+      },
+      y: {
+        type: "uint32",
+        id: 2
+      }
+    }
+  },
+  UserBMoveEvent: {
+    fields: {
+      to: {
+        type: "Position",
+        id: 1
+      }
+    }
+  },
+  GameStateChangeEvent: {
+    fields: {
+      type: {
+        type: "Type",
+        id: 1
+      }
+    },
+    nested: {
+      Type: {
+        values: {
+          Ready: 0,
+          End: 1
+        }
+      }
+    }
+  },
+  ClientMatchStatusRequest: {
+    fields: {
+      type: {
+        type: "Type",
+        id: 1
+      }
+    },
+    nested: {
+      Type: {
+        values: {
+          Ready: 0,
+          End: 1
+        }
+      }
+    }
+  },
+  GameResultEvent: {
+    fields: {
+      type: {
+        type: "Type",
+        id: 1
+      }
+    },
+    nested: {
+      Type: {
+        values: {
+          Success: 0,
+          Failure: 1
+        }
+      }
+    }
+  },
+  ChatSessionEvent: {
+    fields: {
+      type: {
+        type: "Type",
+        id: 1
+      }
+    },
+    nested: {
+      Type: {
+        values: {
+          Begin: 0,
+          End: 1
+        }
+      }
+    }
+  },
+  MatchMakingRequest: {
+    fields: {
+      type: {
+        type: "Type",
+        id: 1
+      }
+    },
+    nested: {
+      Type: {
+        values: {
+          Begin: 0,
+          Cancel: 1
+        }
+      }
+    }
+  },
+  MatchMakingResponse: {
+    fields: {
+      type: {
+        type: "Type",
+        id: 1
+      }
+    },
+    nested: {
+      Type: {
+        values: {
+          Ready: 0,
+          NotAvailable: 1
+        }
+      }
+    }
+  },
   RawMessage: {
     fields: {
       message: {
@@ -29,6 +153,10 @@ var $root = ($protobuf.roots["default"] || ($protobuf.roots["default"] = new $pr
       timestamp: {
         type: "google.protobuf.Timestamp",
         id: 3
+      },
+      timeRemaining: {
+        type: "google.protobuf.Timestamp",
+        id: 4
       }
     }
   },
